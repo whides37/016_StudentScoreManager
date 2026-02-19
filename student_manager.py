4f0d71408359cb01c645e41c329a12748bb68261
whides37 <w.hides37@gmail.com>
2026-02-19 11:08:59 +0900

    点数登録メソッドまで

--- student_manager.py
+++ student_manager.py
@@ -9,5 +9,18 @@ class StudentManager:
             raise ValueError("既に登録されています")
         else:
             self.students[name] = {}  # 空の情報を持つ学生として登録
-            
+
     #削除
+    def remove_student(self,name):
+        if name not in self.students:
+            raise ValueError("登録なしです")
+        del self.students[name]
+
+    #点数登録
+    def add_score(self,name, score):
+        if name not in self.students:
+            raise ValueError("登録のない生徒です")
+        if not isinstance(score,int):
+            raise ValueError("点数は半角数字で入力してください")
+        else:
+            self.students[name].append(score)
